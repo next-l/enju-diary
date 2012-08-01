c861899bf713ac4767260e84ee518300a2224726
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-08-01 23:20:43 +0900

    4日目の作業。最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnjuDiary::Application.config.secret_token = '0bcaa2468a12533392745e09181b0fac8d3185a12440a45744f9cfbd3d1410c0949ba444eac58a5fd5f5d42e103837a69c8d1661747cc11033b71a3c942484c7'
